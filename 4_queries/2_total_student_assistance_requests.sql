c401e94ff7cf3a36e816f4a37954f5fdd1761443
Roderick Nuque <enuke4@gmail.com>
2022-01-05 00:56:13 +0000

    add 2_total_student_assistance_requests.sql file - run and tested - expected result - achieved

---
+++ 4_queries/2_total_student_assistance_requests.sql
@@ -0,0 +1,6 @@
+SELECT students.name AS name,
+COUNT(assistance_requests.*) AS total_assistances
+FROM assistance_requests
+JOIN students ON assistance_requests.student_id = students.id 
+WHERE students.name = 'Elliot Dickinson'
+GROUP BY students.name;
