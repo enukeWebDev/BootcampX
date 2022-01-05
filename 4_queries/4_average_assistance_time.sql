2f69f4a1dba20b418ddde2a59851c847fce760bf
Roderick Nuque <enuke4@gmail.com>
2022-01-05 01:13:33 +0000

    add 5_average_cohort_assistance_time.sql - run & tested - expected result - achieved and modified 4_average_assistance_time.sql

--- 4_queries/4_average_assistance_time.sql
+++ 4_queries/4_average_assistance_time.sql
@@ -1,2 +1,3 @@
-SELECT AVG(assistance_requests.completed_at - assistance_requests.started_at) AS average_assistance_request_duration
+SELECT AVG(assistance_requests.completed_at - assistance_requests.started_at) 
+AS average_assistance_request_duration
 FROM assistance_requests;
